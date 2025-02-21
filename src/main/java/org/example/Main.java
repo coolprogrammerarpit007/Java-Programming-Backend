4fe24e0d8f304396514de90e5ca4860c60c6b736
arpit mishra <arpit.mishra.out@gmail.com>
2025-02-21 07:49:23 +0530

    Java Programming Fundamentals valriables and comments

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,64 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+////            System.out.println("i = " + i);
+//            System.out.println("I am going to master the backend development!");
+//        }
+
+//        variables = a reusable container for a value.
+//                      a variable behaves as if it was the value it contains.
+
+//          Primitive = simple value stored directly in the memory(stack)
+//          Refrence = memory address(stack) that points to the Heap.
+
+//        Primitive Vs Refrence
+//        ---------    --------
+//        int          String
+//        double       array
+//        char         object
+//        boolean
+
+//        Two steps to creata a variable
+//        1. declaration
+//        2. Assignment
+
+        int personAge = 25;
+        System.out.println("My name is Arpit and I am " + personAge + " years old! ");
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+        System.out.println(price);
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isLying = false;
+
+        if(isStudent)
+        {
+            System.out.println("This is a Student");
+        }
+        else
+        {
+            System.out.println("Not a student");
+        }
+
+        String name = "Bro Code";
+        System.out.println(name);
+    }
+}
